f18bccccb33d12b034d2765d9bfe1c767575c979
yangpili114 <yangpili114@gmail.com>
2017-06-29 14:25:58 +0800

    refactor

--- run.php
+++ run.php
@@ -1,30 +1,41 @@
 <?php
+  ini_set('memory_limit', '-1');
+  date_default_timezone_set('Asia/Taipei');
+  require './vendor/autoload.php';
 
-require './vendor/autoload.php';
-$file = './tmp/test.xlsx';
+  ### USL7
+  $file = './tmp/USL7.xlsx';
+  $min_ins_amount = 5000;
+  $max_ins_amount = 7000;
+  $distance = 1000;
+  $ins_amount_pos = 'R8';
+  $ins_fee_pos = 'J19';
 
-try {
- $objPHPExcel = PHPExcel_IOFactory::load($file);
-} catch(Exception $e) {
- die('Error loading file "'.pathinfo($file,PATHINFO_BASENAME).'": '.$e->getMessage());
-}
+  ### FID
+  // $file = './tmp/FID.xlsx';
+  // $ins_amount = 1;
+  // $ins_amount_pos = 'J36';
+  // $ins_fee_pos = 'R34';
 
-$getSheet = $objPHPExcel->getActiveSheet();
-$value = $getSheet->getCell('B2')->getValue();
-echo $getSheet->getCell('A2')->getCalculatedValue()."\n";
-echo $getSheet->getCell('A3')->getCalculatedValue()."\n";
-echo $getSheet->getCell('B2')->getValue()."\n";
-echo $getSheet->getCell('B2')->getCalculatedValue()."\n";
-echo $getSheet->getCell('B2')->getFormattedValue()."\n";
+  try {
+   $objPHPExcel = PHPExcel_IOFactory::load($file);
+  } catch(Exception $e) {
+   die('Error loading file "'.pathinfo($file,PATHINFO_BASENAME).'": '.$e->getMessage());
+  }
+  PHPExcel_Calculation::getInstance($objPHPExcel)->disableCalculationCache();
 
-echo "\n";
+  $getSheet = $objPHPExcel->getActiveSheet();
+  $ins_amount = $min_ins_amount;
+  while ($ins_amount <= $max_ins_amount) {
+    echo "{$ins_amount_pos}: ".$getSheet->getCell($ins_amount_pos)->getCalculatedValue()."\n";
+    echo "{$ins_fee_pos}: ".$getSheet->getCell($ins_fee_pos)->getCalculatedValue()."\n";
+    $getSheet->setCellValue($ins_amount_pos,$ins_amount);
 
-$getSheet->setCellValue('A2',12);
-PHPExcel_Calculation::getInstance( $getSheet->getParent() )->flushInstance();
-echo $getSheet->getCell('A2')->getCalculatedValue()."\n";
-echo $getSheet->getCell('A3')->getCalculatedValue()."\n";
-echo $getSheet->getCell('B2')->getValue()."\n";
-echo $getSheet->getCell('B2')->getCalculatedValue()."\n";
-echo $getSheet->getCell('B2')->getFormattedValue()."\n";
+    echo "{$ins_amount_pos}: ".$getSheet->getCell($ins_amount_pos)->getCalculatedValue()."\n";
+    echo "{$ins_fee_pos}: ".$getSheet->getCell($ins_fee_pos)->getCalculatedValue()."\n";
+    echo "\n";
 
+    $ins_amount = $ins_amount + $distance;
+    echo "============================\n";
+  }
 ?>
