8328b41a4f637083046ad15cbf4a220aedf7adb7
yangpili114 <yangpili114@gmail.com>
2017-11-09 15:16:51 +0800

    add samples

--- README.md
+++ README.md
@@ -22,6 +22,13 @@ php composer.phar install
 php run.php
 ```
 
+## Sample file
+
+- DY1: 遠雄-美滿永富利率變動型增額終身壽險
+- ISJ: 宏泰-泰富貴利率變動型終身壽險試算表
+- EW:  元大-億達利率變動型增額終身壽險
+- USL7:台灣人壽吉美鑫美元利率變動型增額終身壽險
+
 ## Reference
 
 - [PHPExcel won't recalculate [duplicate]](http://stackoverflow.com/questions/16009822/phpexcel-wont-recalculate)
--- run.php
+++ run.php
@@ -4,19 +4,38 @@
   require './vendor/autoload.php';
 
   ### USL7
-  $file = './tmp/USL7.xlsx';
-  $min_ins_amount = 5000;
-  $max_ins_amount = 7000;
-  $distance = 1000;
-  $ins_amount_pos = 'R8';
-  $ins_fee_pos = 'J19';
+  // $file = './tmp/USL7.xlsx';
+  // $min_ins_amount = 5000;
+  // $max_ins_amount = 7000;
+  // $distance = 1000;
+  // $ins_amount_pos = 'R8';
+  // $ins_fee_pos = 'J19';
 
-  ### FID
-  // $file = './tmp/FID.xlsx';
-  // $ins_amount = 1;
-  // $ins_amount_pos = 'J36';
-  // $ins_fee_pos = 'R34';
+  ### DY1
+  // $file = './tmp/DY1.xls';
+  // $min_ins_amount = 100;
+  // $max_ins_amount = 300;
+  // $distance = 100;
+  // $ins_amount_pos = 'I15';
+  // $ins_fee_pos = 'I47';
 
+  ### ISJ
+  // $file = './tmp/ISJ.xlsx';
+  // $min_ins_amount = 100;
+  // $max_ins_amount = 300;
+  // $distance = 100;
+  // $ins_amount_pos = 'P10';
+  // $ins_fee_pos = 'T10';
+
+  ### EW
+  $file = './tmp/EW.xls';
+  $min_ins_amount = 100;
+  $max_ins_amount = 300;
+  $distance = 100;
+  $ins_amount_pos = 'J8';
+  $ins_fee_pos = 'H16';
+
+  echo "Filepath: {$file}\n";
   try {
    $objPHPExcel = PHPExcel_IOFactory::load($file);
   } catch(Exception $e) {
@@ -27,8 +46,8 @@
   $getSheet = $objPHPExcel->getActiveSheet();
   $ins_amount = $min_ins_amount;
   while ($ins_amount <= $max_ins_amount) {
-    echo "{$ins_amount_pos}: ".$getSheet->getCell($ins_amount_pos)->getCalculatedValue()."\n";
-    echo "{$ins_fee_pos}: ".$getSheet->getCell($ins_fee_pos)->getCalculatedValue()."\n";
+    // echo "{$ins_amount_pos}: ".$getSheet->getCell($ins_amount_pos)->getCalculatedValue()."\n";
+    // echo "{$ins_fee_pos}: ".$getSheet->getCell($ins_fee_pos)->getCalculatedValue()."\n";
     $getSheet->setCellValue($ins_amount_pos,$ins_amount);
 
     echo "{$ins_amount_pos}: ".$getSheet->getCell($ins_amount_pos)->getCalculatedValue()."\n";
